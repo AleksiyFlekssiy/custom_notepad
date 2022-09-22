3d005edd05ad5411f031e0f11cdbfa9b025a440d
AleksiyFlekssiy <70132230+AleksiyFlekssiy@users.noreply.github.com>
2022-09-23 00:29:10 +0300

    Finally got working JAR file.
    Maven isn't useful in my project.... at least now.

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.custom_notepad {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.custom_notepad to javafx.fxml;
-    exports com.example.custom_notepad;
-}
